da3c4c1af960724d397b2b01416fb4d88ae68c0a
Lucas Toti <lucas.marinho@prof.totidiversidade.com.br>
2023-07-04 15:58:18 -0300

    atualizacao depois da primeira aula

--- src/script.js
+++ src/script.js
@@ -3,3 +3,5 @@ const message = 'Meu arquivo externo';
 // Atualizar o header
 document.querySelector('#header').innerHTML = message;
 console.log(message);
+
+document.querySelector('#content').innerHTML = 'Turma 33 - Aula Javascript';
